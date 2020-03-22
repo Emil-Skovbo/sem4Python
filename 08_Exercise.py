90f17bd05674257edf5acacb508afac6fec6aa49
Emil <emilskovbo18@gmail.com>
2020-03-23 00:36:00 +0100

    exercise 08

--- 08_Exercise.py
+++ 08_Exercise.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
 import datetime
+from datetime import datetime, date, timedelta
+from decimal import Decimal
 import pymysql
 
 cnx = pymysql.connect(user='dev', password='ax2',
@@ -17,3 +20,46 @@ for (id, firstname) in cursor:
 
 cursor.close()
 cnx.close()
+
+
+# Connect with the MySQL Server
+cnx = pymysql.connect(user='dev', password='ax2',
+                      host='127.0.0.1', port=3307, db='pythondemo')
+cursor = cnx.cursor()
+
+curA = cnx.cursor()
+curB = cnx.cursor()
+
+# Query to get employees who joined in a period defined by two dates
+query = ("SELECT id, salary FROM pythondemo WHERE enddate IS NULL")
+
+# UPDATE and INSERT statements for the old and new salary
+update_old_salary = (
+    "UPDATE pythondemo SET salary = %s "
+    "WHERE id = %s")
+
+# Select the employees getting a raise (all that are still employed)
+curA.execute(query)
+
+# Iterate through the result of curA
+for (id, salary) in curA:
+    # Update the old and insert the new salary
+    new_salary = int(round(Decimal(salary) * Decimal('1.15')))
+    curB.execute(update_old_salary, (new_salary, id))
+    # Commit the changes
+    cnx.commit()
+cursor.close()
+curA.close()
+curB.close()
+cnx.close()
+
+
+cnx = pymysql.connect(user='dev', password='ax2',
+                      host='127.0.0.1', port=3307, db="pythondemo")
+
+cursor = cnx.cursor(pymysql.cursors.DictCursor)
+
+query = ("SELECT firstname, lastname, startdate, enddate, salary FROM pythondemo")
+
+cursor.execute(query)
+cursor.fetchall()
